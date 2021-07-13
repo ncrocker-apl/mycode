a3a08f41c3cbfc415ab2fcd69801d5867338e1b4
ncrocker-apl <59446483+ncrocker-apl@users.noreply.github.com>
2021-07-13 13:10:46 -0500

    Update lab19DHCPbuild.sh
    Missed bowser-dhcp

--- lab19DHCPbuild.sh
+++ lab19DHCPbuild.sh
@@ -6,6 +6,7 @@ sudo ip netns add dhcp-bowser
 
 # create OVS-internal interfaces
 sudo ovs-vsctl add-port donut-plains dhcp-peach -- set interface dhcp-peach type=internal
+sudo ovs-vsctl add-port donut-plains dhcp-bowser -- set interface dhcp-bowser type=internal
 
 # apply VLAN tags
 sudo ovs-vsctl set port dhcp-peach tag=50
